db79d1c93b198d0d78b21373e5eb5c4c29f87f5f
Ali <ak@e-switch.ch>
2024-04-05 16:54:27 +0200

    enable cors

--- customer-management-backend/src/main/java/com/lakesidemutual/customermanagement/interfaces/configuration/WebConfiguration.java
+++ customer-management-backend/src/main/java/com/lakesidemutual/customermanagement/interfaces/configuration/WebConfiguration.java
@@ -8,15 +8,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 /**
- * The WebConfiguration class is used to customize the default Spring MVC configuration.
- * */
+ * The WebConfiguration class is used to customize the default Spring MVC
+ * configuration.
+ */
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 	/**
-	 * This web servlet makes the web console of the H2 database engine available at the "/console" endpoint.
-	 * */
+	 * This web servlet makes the web console of the H2 database engine available at
+	 * the "/console" endpoint.
+	 */
 	@Bean
 	public ServletRegistrationBean<WebServlet> h2servletRegistration() {
 		ServletRegistrationBean<WebServlet> registrationBean = new ServletRegistrationBean<>(new WebServlet());
@@ -25,9 +28,11 @@ public class WebConfiguration implements WebMvcConfigurer {
 	}
 
 	/**
-	 * This is a filter that generates an ETag value based on the content of the response. This ETag is compared to the If-None-Match header
-	 * of the request. If these headers are equal, the response content is not sent, but rather a 304 "Not Modified" status instead.
-	 * */
+	 * This is a filter that generates an ETag value based on the content of the
+	 * response. This ETag is compared to the If-None-Match header
+	 * of the request. If these headers are equal, the response content is not sent,
+	 * but rather a 304 "Not Modified" status instead.
+	 */
 	@Bean
 	public Filter shallowETagHeaderFilter() {
 		return new ShallowEtagHeaderFilter();
